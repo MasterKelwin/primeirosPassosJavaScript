d5b03bc2c967a798fa10340fbbdbe32f4e4d6e99
MasterKelwin <mkelwin@outlook.com>
2022-01-21 10:44:12 -0300

    entendendo variáveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com atribuição de variáveis");
+//JS é case sensitive
+
+const idade = 29;
+let nome = "Kelwin";
+const sobrenome = "Hollunder";  //CONST são variáveis que não podem ser alteradas!
+
+//console.log(nome + sobrenome);
+//console.log(nome, sobrenome);
+//console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é ${nome} ${sobrenome}`); // "${} para declarar variáveis dentro de strings"
+
+nome = `Meu nome é ${nome} ${sobrenome}`
+
+console.log(nome)
+
+
+
+//Usar const sempre que possível é parte de Boas Práticas.
