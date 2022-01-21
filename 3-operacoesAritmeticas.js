067c5b999f0289264967a78d84b3d3d9e5b05905
MasterKelwin <mkelwin@outlook.com>
2022-01-21 09:53:49 -0300

    Básico do básico

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,11 @@
+console.log("Operações Aritiméticas");
+
+console.log(2+2);
+console.log(10 + 8 * 2);
+console.log((10 + 8)* 2);
+console.log("ano" + 2020)
+console.log("2" + "2")
+
+//Ou sempre use ; no final das linhas ou nunca use.
+//outras linguagens podem exigir o uso do ;.
+//tudo entre "" se torna uma STRING.
