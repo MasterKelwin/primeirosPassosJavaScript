c4fae0a58ada973294593b673b2ea5b818e15d15
MasterKelwin <mkelwin@outlook.com>
2022-01-21 10:20:30 -0300

    entendendo parseFloat e parseInt

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("7" / "2");
+console.log("nome" / "2"); //Not a Number = NaN
+
+console.log(7,5); //Vírgula tem caráter separador
+console.log(7.5); //Pontos para números racionais
