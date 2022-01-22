3756abb8d93772be27942ea47b5df860e317345b
MasterKelwin <mkelwin@outlook.com>
2022-01-22 13:03:50 -0300

    .

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,19 +1,25 @@
 console.log("Trabalhando com atribuição de variáveis");
 //JS é case sensitive
 
-const idade = 29;
+//const idade = 29;
 let nome = "Kelwin";
 const sobrenome = "Hollunder";  //CONST são variáveis que não podem ser alteradas!
 
 //console.log(nome + sobrenome);
 //console.log(nome, sobrenome);
 //console.log(nome + " " + sobrenome);
+//console.log(`Meu nome é ${nome} ${sobrenome}`); // "${} para declarar variáveis dentro de strings"
 
-console.log(`Meu nome é ${nome} ${sobrenome}`); // "${} para declarar variáveis dentro de strings"
+nome = `Meu nome é ${nome} ${sobrenome}`;
 
-nome = `Meu nome é ${nome} ${sobrenome}`
+console.log(nome);
 
-console.log(nome)
+let idade; //declarando a variavel
+let anosDesdeJulhoDe2020;
+idade = 23; //atribuindo a variavel
+anosDesdeJulhoDe2020 = 1;
+idade = idade + anosDesdeJulhoDe2020; //somando variaveis
+console.log(idade);
 
 
 
