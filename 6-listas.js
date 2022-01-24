acb92d4cc553add9a02ff070d5aa5c8955fa2130
MasterKelwin <mkelwin@outlook.com>
2022-01-24 10:45:38 -0300

    Aprendendo Array e Push

---
+++ 6-listas.js
@@ -0,0 +1,16 @@
+console.log(`Trabalhando com listas`);
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`
+
+const listaDeDestinos = new Array(  //declarando uma lista
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) //adicionando um item na lista após ter sido criada
+
+console.log(`Destinos possíveis`)
+console.log(listaDeDestinos);
