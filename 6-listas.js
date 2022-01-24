9a312bdd32a7d5321c91cc37a09b5c7bf37e698d
MasterKelwin <mkelwin@outlook.com>
2022-01-24 11:00:21 -0300

    array, push, splice, imprimir no console e aprendizado sobre palavras reservadas da linguagem

--- 6-listas.js
+++ 6-listas.js
@@ -10,7 +10,9 @@ const listaDeDestinos = new Array(  //declarando uma lista
     `Rio de Janeiro`
 );
 
-listaDeDestinos.push(`Curitiba`) //adicionando um item na lista após ter sido criada
+listaDeDestinos.push(`Curitiba`, `Afonso Cláudio`); //adicionando um item na lista após ter sido criada
+listaDeDestinos.splice(1,1);  //para remover itens do meu array, dentro do splice o primeiro número é referente a posição de inicio e o segundo número é sobre o número de itens a serem deletados
 
-console.log(`Destinos possíveis`)
+console.log(`Destinos possíveis`);
 console.log(listaDeDestinos);
+console.log(listaDeDestinos[1], listaDeDestinos[2]); //entre [] para declararmos qual valor deve ser exibido
